bef52ce4ecec0f0b08b31c6e745ee56d21c9a231
GBOLUTS <bol_gena@mail.ru>
2018-03-06 20:19:06 +0700

    1) Список сайтов берётся из веб-сервиса 2) Т.к. Веб-сервис не даёт информацию, то на основании сайтов из п.1 создаются фейковые данные по посещению каждого сайта

--- src/com/company/FixedConnector.java
+++ src/com/company/FixedConnector.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
  * Created by Гена on 13.02.18.
  */
 public class FixedConnector implements AbstractConnector {
+    SiteConnector siteConnector = new SiteConnector();
     @Override
     public String[] findListOfSites() {
         String[] listOfSites = {"www.lenta.ru","www.vk.com","www.mail.ru"};
@@ -16,15 +17,22 @@ public class FixedConnector implements AbstractConnector {
     public ArrayList<Pretendents> findListOFPeopleWithoutDate()
     {
         ArrayList <Pretendents> listOfPretendentsWithoutDate = new ArrayList<Pretendents>();
-        listOfPretendentsWithoutDate.add( new Pretendents("www.lenta.ru","Путин",5));
-        listOfPretendentsWithoutDate.add( new Pretendents("www.lenta.ru","Медведев",7));
-        listOfPretendentsWithoutDate.add( new Pretendents("www.lenta.ru","Навальный",12));
-        listOfPretendentsWithoutDate.add( new Pretendents("www.vk.com","Путин",6));
-        listOfPretendentsWithoutDate.add( new Pretendents("www.vk.com","Медведев",8));
-        listOfPretendentsWithoutDate.add( new Pretendents("www.vk.com","Навальный",14));
-        listOfPretendentsWithoutDate.add( new Pretendents("www.mail.ru","Путин",2));
-        listOfPretendentsWithoutDate.add( new Pretendents("www.mail.ru","Медведев",9));
-        listOfPretendentsWithoutDate.add( new Pretendents("www.mail.ru","Навальный",7));
+        String [] listOfSitesFromWebServer = siteConnector.findListOfSites();
+        for (int i = 0; i < listOfSitesFromWebServer.length; i++) {
+            listOfPretendentsWithoutDate.add(new Pretendents(listOfSitesFromWebServer[i],"Путин", (i + 1)*4));
+            listOfPretendentsWithoutDate.add(new Pretendents(listOfSitesFromWebServer[i],"Медведев", i *3));
+            listOfPretendentsWithoutDate.add(new Pretendents(listOfSitesFromWebServer[i],"Навальный", i + 2));
+        }
+
+//        listOfPretendentsWithoutDate.add( new Pretendents("turner.ca","Путин",5));
+//        listOfPretendentsWithoutDate.add( new Pretendents("turner.ca","Медведев",7));
+//        listOfPretendentsWithoutDate.add( new Pretendents("turner.ca","Навальный",12));
+//        listOfPretendentsWithoutDate.add( new Pretendents("www.vk.com","Путин",6));
+//        listOfPretendentsWithoutDate.add( new Pretendents("www.vk.com","Медведев",8));
+//        listOfPretendentsWithoutDate.add( new Pretendents("www.vk.com","Навальный",14));
+//        listOfPretendentsWithoutDate.add( new Pretendents("www.mail.ru","Путин",2));
+//        listOfPretendentsWithoutDate.add( new Pretendents("www.mail.ru","Медведев",9));
+//        listOfPretendentsWithoutDate.add( new Pretendents("www.mail.ru","Навальный",7));
         return listOfPretendentsWithoutDate;
     }
 
@@ -32,26 +40,39 @@ public class FixedConnector implements AbstractConnector {
     public ArrayList<Pretendents> findListOFPeopleWithDate()
     {
         ArrayList <Pretendents> listOfPretendentsWithDate = new ArrayList<>();
-        listOfPretendentsWithDate.add( new Pretendents("www.lenta.ru","Путин",5,"01.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.lenta.ru","Медведев",7,"01.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.lenta.ru","Навальный",12,"01.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.vk.com","Путин",6,"01.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.vk.com","Медведев",8,"01.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.vk.com","Навальный",14,"01.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.mail.ru","Путин",2,"01.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.mail.ru","Медведев",9,"01.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.mail.ru","Навальный",7,"01.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.lenta.ru","Путин",5,"02.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.lenta.ru","Медведев",7,"02.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.lenta.ru","Навальный",12,"02.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.vk.com","Путин",6,"02.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.vk.com","Медведев",8,"02.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.vk.com","Навальный",14,"02.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.mail.ru","Путин",2,"02.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.mail.ru","Медведев",9,"02.02.2018"));
-        listOfPretendentsWithDate.add( new Pretendents("www.mail.ru","Навальный",7,"02.02.2018"));
+        String [] listOfSitesFromWebServer = siteConnector.findListOfSites();
+
+        for (int i = 0; i < listOfSitesFromWebServer.length; i++) {
+            listOfPretendentsWithDate.add(new Pretendents(listOfSitesFromWebServer[i],"Путин", (i + 1)*4, "01.02.2018"));
+            listOfPretendentsWithDate.add(new Pretendents(listOfSitesFromWebServer[i],"Медведев", i *3,"01.02.2018" ));
+            listOfPretendentsWithDate.add(new Pretendents(listOfSitesFromWebServer[i],"Навальный", i + 2, "01.02.2018"));
+            listOfPretendentsWithDate.add(new Pretendents(listOfSitesFromWebServer[i],"Путин", (i + 3)*5, "02.02.2018"));
+            listOfPretendentsWithDate.add(new Pretendents(listOfSitesFromWebServer[i],"Медведев", i *3+1,"02.02.2018" ));
+            listOfPretendentsWithDate.add(new Pretendents(listOfSitesFromWebServer[i],"Навальный", i*2 + 2, "02.02.2018"));
+            listOfPretendentsWithDate.add(new Pretendents(listOfSitesFromWebServer[i],"Путин", (3*i + 1)*4, "03.02.2018"));
+            listOfPretendentsWithDate.add(new Pretendents(listOfSitesFromWebServer[i],"Медведев", i+4,"03.02.2018" ));
+            listOfPretendentsWithDate.add(new Pretendents(listOfSitesFromWebServer[i],"Навальный", 5*i +1, "03.02.2018"));
+        }
 
 
+//        listOfPretendentsWithDate.add( new Pretendents("www.lenta.ru","Путин",5,"01.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.lenta.ru","Медведев",7,"01.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.lenta.ru","Навальный",12,"01.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.vk.com","Путин",6,"01.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.vk.com","Медведев",8,"01.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.vk.com","Навальный",14,"01.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.mail.ru","Путин",2,"01.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.mail.ru","Медведев",9,"01.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.mail.ru","Навальный",7,"01.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.lenta.ru","Путин",5,"02.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.lenta.ru","Медведев",7,"02.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.lenta.ru","Навальный",12,"02.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.vk.com","Путин",6,"02.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.vk.com","Медведев",8,"02.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.vk.com","Навальный",14,"02.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.mail.ru","Путин",2,"02.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.mail.ru","Медведев",9,"02.02.2018"));
+//        listOfPretendentsWithDate.add( new Pretendents("www.mail.ru","Навальный",7,"02.02.2018"));
 
         return listOfPretendentsWithDate;
     }
